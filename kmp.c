90646483513c8c4935f998622cf5d6cfb7704f0b
donuts2024 <t8576860@gmail.com>
2024-05-18 01:18:34 +0530

    Add files via upload

---
+++ kmp.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <string.h>
+
+void computeLPSArray(char *pat, int M, int *lps);
+void KMPSearch(char *pat, char *txt);
+
+void computeLPSArray(char *pat, int M, int *lps)
+{
+    int len = 0;
+    lps[0] = 0;
+
+    int i = 1;
+    while (i < M)
+    {
+        if (pat[i] == pat[len])
+        {
+            len++;
+            lps[i] = len;
+            i++;
+        }
+        else
+        {
+            if (len != 0)
+            {
+                len = lps[len - 1];
+            }
+            else
+            {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+}
+
+void KMPSearch(char *pat, char *txt)
+{
+    int M = strlen(pat);
+    int N = strlen(txt);
+
+    int lps[M];
+    computeLPSArray(pat, M, lps);
+
+    int i = 0;
+    int j = 0;
+    int found = 0; // Flag to indicate if pattern is found
+
+    while (i < N)
+    {
+        if (pat[j] == txt[i])
+        {
+            j++;
+            i++;
+        }
+
+        if (j == M)
+        {
+            printf("Pattern found at index %d\n", i - j);
+            j = lps[j - 1];
+            found = 1; // Set flag to true
+        }
+        else if (i < N && pat[j] != txt[i])
+        {
+            if (j != 0)
+                j = lps[j - 1];
+            else
+                i++;
+        }
+    }
+
+    if (!found)
+    {
+        printf("Pattern not found in the text.\n");
+    }
+}
+int main()
+{
+    char txt[100], pat[100];
+    printf("Enter the text: ");
+    scanf("%s", txt);
+    printf("Enter the pattern: ");
+    scanf("%s", pat);
+
+    KMPSearch(pat, txt);
+
+    return 0;
+}
